521773f330910cc1c3f5093a592d23bb2979389e
kenken <kengoviolinphoto@gmail.com>
2024-05-19 19:36:55 +0900

    first commit

---
+++ lib/work-art/findy-logo.rb
@@ -0,0 +1,43 @@
+def setup
+  createCanvas(400, 400)
+end
+
+def draw
+  background(220, 255, 255)
+  # color
+  gray = color(165, 165, 164)
+  white = color(255, 255, 255)
+  blue = color(26, 90, 164)
+  # config
+  noStroke()
+
+  # 外周
+  fill(gray)
+  arc(200, 200, 300, 300, radians(0), radians(360), PIE)
+  fill(blue)
+  arc(200, 200, 300, 300, radians(130), radians(300), PIE)
+  fill(gray)
+  arc(284, 115.5, 150, 90, radians(190), radians(260), PIE)
+  fill(blue)
+  arc(95, 270.5, 150, 90, radians(-60), radians(80), PIE)
+
+
+  # くり抜き
+  fill(white)
+  circle(200, 200, 210)
+
+  # Fの横線
+  fill(blue)
+  rotate(-0.15)
+  arc(127, 270, 200, 200, radians(190), radians(290), PIE)
+  fill(white)
+  arc(127, 270, 120, 120, radians(190), radians(360), PIE)
+  arc(190, 231, 125, 125, radians(200), radians(250), PIE)
+
+  # 取手部分
+  fill(gray)
+  translate(width / 1.25, height / 1.25)
+  rotate(radians(55))
+  rectMode(CENTER)
+  rect(-5, 75, 70, 45)
+end
